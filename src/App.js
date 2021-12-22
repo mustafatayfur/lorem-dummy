a49ebb708b791cffbf9b1fb0f6c030291fe08371
mustafa <mltm0204@gmail.com>
2021-12-22 14:34:39 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
 
   const handleSubmit = (e)=> {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count);
+    if(count <= 0){
+      amount= 1
+    }
+    if(count > 8){
+      amount= 8
+    }
+    
+    setText(data.slice(0, amount));
   }
   
   return (
@@ -22,7 +30,11 @@ function App() {
             <button type='submit' className='btn'>generate</button>
         </form>
         <article className='lorem-text'>
-            {}
+            {text.map((item, index)=>{
+              return (
+                <p key={index}>{item}</p>
+              )
+            })}
         </article>
     </section>
   )
